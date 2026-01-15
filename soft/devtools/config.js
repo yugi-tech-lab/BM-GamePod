1c480c876e90452f44926cb1ae6519a3785ea1bc
Yugi Tech Lab <yugi.tech.lab@gmail.com>
2026-01-15 23:24:25 +0900

    Add files via upload

--- soft/devtools/config.js
+++ soft/devtools/config.js
@@ -7,4 +7,4 @@ const PC_URL = "http://XX.XXX.XXX.XX:8000";
 // GitHubリポジトリURL（任意）
 // 例: "https://github.com/<yourname>/BM-GamePod"
 // 空のままでも動きます（リンクは非表示になります）
-const GITHUB_REPO_URL = "";
+const GITHUB_REPO_URL = "https://github.com/yugi-tech-lab/BM-GamePod";
